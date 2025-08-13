bad2f24980dda38a78591cb393035a9d6e551943
Massamba505 <sabelomaphalala131@gmail.com>
2025-08-13 16:08:10 +0200

    refactor: restore PhantomGGContext with updated identity models

--- Server/PhantomGG.API/Data/PhantomGGContext.cs
+++ Server/PhantomGG.API/Data/PhantomGGContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using PhantomGG.API.Models;
 
-namespace PhantomGG.API.Models;
+namespace PhantomGG.API.Data;
 
 public partial class PhantomGGContext : DbContext
 {
