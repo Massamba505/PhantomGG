2942a003b57e0bf0b7ceeac48b7a179e70d4e3f9
Massamba505 <sabelomaphalala131@gmail.com>
2025-08-14 08:52:51 +0200

    aspnet

--- Server/PhantomGG.API/Models/AspNetUser.cs
+++ Server/PhantomGG.API/Models/AspNetUser.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace PhantomGG.API.Models;
 
-public partial class AspNetUser
+public partial class AspNetUser : IdentityUser<Guid>
 {
-    public Guid Id { get; set; }
-
     public string FirstName { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
@@ -16,34 +15,6 @@ public partial class AspNetUser
     public DateTime CreatedAt { get; set; }
 
     public bool IsActive { get; set; }
-
-    public string? UserName { get; set; }
-
-    public string? NormalizedUserName { get; set; }
-
-    public string? Email { get; set; }
-
-    public string? NormalizedEmail { get; set; }
-
-    public bool EmailConfirmed { get; set; }
-
-    public string? PasswordHash { get; set; }
-
-    public string? SecurityStamp { get; set; }
-
-    public string? ConcurrencyStamp { get; set; }
-
-    public string? PhoneNumber { get; set; }
-
-    public bool PhoneNumberConfirmed { get; set; }
-
-    public bool TwoFactorEnabled { get; set; }
-
-    public DateTimeOffset? LockoutEnd { get; set; }
-
-    public bool LockoutEnabled { get; set; }
-
-    public int AccessFailedCount { get; set; }
 
     public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; } = new List<AspNetUserClaim>();
 
