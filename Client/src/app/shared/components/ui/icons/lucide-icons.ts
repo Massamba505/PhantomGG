d16e915b52f92f1a6835658a6c74105221a1f233
Massamba505 <sabelomaphalala131@gmail.com>
2025-08-26 10:00:24 +0200

    Lucide Icons

---
+++ Client/src/app/shared/components/ui/icons/lucide-icons.ts
@@ -0,0 +1,31 @@
+import {
+  Menu,
+  Sun,
+  Moon,
+  LayoutGrid,
+  StretchHorizontal,
+  Search,
+  Funnel,
+  SquarePen,
+  Trash2,
+  User,
+  Users,
+  Timer,
+  CalendarDays
+} from 'lucide-angular';
+
+export const LucideIcons = {
+  Menu,
+  Sun,
+  Moon,
+  LayoutGrid,
+  StretchHorizontal,
+  Search,
+  Funnel,
+  SquarePen,
+  Trash2,
+  User,
+  Users,
+  Timer,
+  CalendarDays
+};
