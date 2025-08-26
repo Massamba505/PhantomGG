13657227ec7a92622dfdb4ca8cf18922e15faa26
Massamba505 <sabelomaphalala131@gmail.com>
2025-08-26 18:37:12 +0200

    Add DTOs for Team and Tournament management

---
+++ Server/PhantomGG.API/DTOs/Team/UpdateTeamDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PhantomGG.API.DTOs.Team;
+
+public class UpdateTeamDto
+{
+    [Required]
+    [StringLength(200, MinimumLength = 2)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string Manager { get; set; } = string.Empty;
+
+    [Range(1, 50)]
+    public int NumberOfPlayers { get; set; }
+
+    [Url]
+    public string? LogoUrl { get; set; }
+}
---
+++ Server/PhantomGG.API/DTOs/Tournament/UpdateTournamentDto.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PhantomGG.API.DTOs.Tournament;
+
+public class UpdateTournamentDto
+{
+    [Required]
+    [StringLength(200, MinimumLength = 3)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(2000, MinimumLength = 10)]
+    public string Description { get; set; } = string.Empty;
+
+    [StringLength(200)]
+    public string? Location { get; set; }
+
+    public DateTime? RegistrationDeadline { get; set; }
+
+    [Required]
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    public DateTime EndDate { get; set; }
+
+    [Range(4, 128)]
+    public int MaxTeams { get; set; }
+
+    [Range(0, 999999.99)]
+    public decimal? EntryFee { get; set; }
+
+    [Range(0, 999999.99)]
+    public decimal? Prize { get; set; }
+
+    [EmailAddress]
+    public string? ContactEmail { get; set; }
+
+    [Url]
+    public string? BannerUrl { get; set; }
+
+    [Required]
+    public string Status { get; set; } = string.Empty;
+}
